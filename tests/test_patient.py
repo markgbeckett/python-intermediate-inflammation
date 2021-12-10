888c9e402b06f0f2f0e64cd58477562ac111066d
G Francis <g.francis@epcc.ed.ac.uk>
2021-12-10 12:29:22 +0000

    fix for #7 - add __eq__ for Person

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -8,3 +8,16 @@ def test_create_patient():
     p = Patient(name=name)
 
     assert p.name == name
+
+
+def test_patient_eqality():
+    from inflammation.models import Patient
+
+    name1 = 'Alice'
+    name2 = 'Bob'
+    p1 = Patient(name=name1)
+    p2 = Patient(name=name1)
+    p3 = Patient(name=name2)
+
+    assert p1 == p2
+    assert p1 != p3
